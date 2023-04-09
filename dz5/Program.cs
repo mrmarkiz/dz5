bf74bbd6aed2b7e4c75c9cae73abd9bc6c23f967
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-10 00:34:08 +0300

    Add project files.

---
+++ dz5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace dz5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int run;
+            do
+            {
+                Console.Write("Enter task to run (0 to exit): ");
+                int.TryParse(Console.ReadLine(), out run);
+                switch (run)
+                {
+                    case 1:
+                        Task1.Run();
+                        break;
+                    case 2:
+                        Task2.Run();
+                        break;
+                    case 3:
+                        Task3.Run();
+                        break;
+                    case 4:
+                        Task4.Run();
+                        break;
+                }
+            } while (run != 0);
+        }
+    }
+}
